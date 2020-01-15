638170868d5423e88ff2832deb843005130f03bd
Usairim Isani <usairim.isnai@outlook.com>
2020-01-15 23:39:12 +0500

    R Project

---
+++ index.r
@@ -0,0 +1,16 @@
+onch = read.csv("dataset.csv")
+onch.dx.groups = read.csv("dx_conversion.csv")
+onch.mean = mean(onch$CASES)
+onch.all_cases = sum(onch$CASES)
+onch.male_cases = onch$CASES[onch$SEX == "Male"]
+onch.male_n = onch$N[onch$SEX == "Male"]
+onch.male_prev = onch.male_cases/ onch.male_n
+onch.male_prev_avg = mean(onch.male_prev,na.rm = T)
+onch.female_cases = onch$CASES[onch$SEX == "Female"]
+onch.female_n = onch$N[onch$SEX == "Female"]
+onch.female_prev = onch.female_cases/ onch.female_n
+onch.female_prev_avg = mean(onch.female_prev,na.rm = T)
+onch.both_cases = onch$CASES[onch$SEX == "Both"]
+onch.ss = onch$CASES[onch$DX_GROUP == "ss"]
+onch.ss_total = sum(onch.ss)
+onch.types_of_dx_cases = c() 
